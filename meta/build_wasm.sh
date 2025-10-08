44d35fe37cf4cdf768596ebee0d6199197009ef8
Mustafa Quraish <mustafaq9@gmail.com>
2025-10-07 21:43:30 -0400

    add workflow to wasm build

---
+++ meta/build_wasm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -xe
+
+mkdir -p build
+
+if [[ -z "$1" ]]; then
+    ocen src/main.oc -n -c build/render.c
+    FILE=build/render.c
+else
+    FILE=$1
+fi
+
+
+cp assets/index.html build/
+emcc $FILE \
+    -O3 \
+    -DOC_NO_BACKTRACE \
+    -s USE_SDL=2 \
+    -s USE_ZLIB=1 \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    --preload-file assets \
+    --use-preload-plugins \
+    -o build/index.js \
+    -s ASYNCIFY=1 \
+    -s DISABLE_DEPRECATED_FIND_EVENT_TARGET_BEHAVIOR=0
